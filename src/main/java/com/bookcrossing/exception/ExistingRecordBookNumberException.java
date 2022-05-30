313b4c8c85f9d4a815e3928f79f7420910f49c92
kyz9 <ikyz9.work@gmail.com>
2022-05-31 03:46:49 +0400

    feat: add Authentication and registration

---
+++ src/main/java/com/bookcrossing/exception/ExistingRecordBookNumberException.java
@@ -0,0 +1,18 @@
+package com.bookcrossing.exception;
+
+public class ExistingRecordBookNumberException extends Throwable {
+    public ExistingRecordBookNumberException() {
+    }
+
+    public ExistingRecordBookNumberException(String message) {
+        super(message);
+    }
+
+    public ExistingRecordBookNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExistingRecordBookNumberException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/bookcrossing/repository/UsersRepository.java
+++ src/main/java/com/bookcrossing/repository/UsersRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UsersModel, Long> {
+public interface UsersRepository extends JpaRepository<UsersModel, Long> {
+    UsersModel findByLogin(String login);
+
+    boolean existsByLogin(String login);
+    boolean existsByEmail(String email);
 }
