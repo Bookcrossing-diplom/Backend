66954a5eae97ad4e4373383ae252f48dec9af4c3
kyz9 <ikyz9.work@gmail.com>
2022-06-01 01:30:53 +0400

    feat: add get mybooks and desired

---
+++ src/main/java/com/bookcrossing/repository/UsersPersonalOfficeInfo.java
@@ -0,0 +1,10 @@
+package com.bookcrossing.repository;
+
+public interface UsersPersonalOfficeInfo {
+    long getId();
+    String getLogin();
+    String getFirstname();
+    String getLastname();
+    String getEmail();
+    String getCity();
+}
