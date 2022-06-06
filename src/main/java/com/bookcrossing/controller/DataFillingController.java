1861e7e4c47d2a0f48b1971653a6c9773f3c6517
kyz9 <ikyz9.work@gmail.com>
2022-06-06 21:24:46 +0400

    fix: data filling

--- src/main/java/com/bookcrossing/controller/DataFillingController.java
+++ src/main/java/com/bookcrossing/controller/DataFillingController.java
@@ -67,25 +67,25 @@ public class DataFillingController {
         CategoryModel categoryModel1 = CategoryModel.builder().name("Классическая литература").genres(Set.of(genreModel1, genreModel2)).build();
         CategoryModel categoryModel2 = CategoryModel.builder().name("Ужасы, мистика").genres(Set.of(genreModel3, genreModel4)).build();
 
-        BookModel bookModel = BookModel.builder().name("Война и мир").edition("если есть много свободного времени").yearPublishing("1990").authors(Set.of(authorModel1)).categories(Set.of(categoryModel)).genres(Set.of(genreModel)).build();
-        BookModel bookModel1 = BookModel.builder().name("Отцы и дети").edition("Как же хорошо вернуться домой").yearPublishing("2006").authors(Set.of(authorModel2)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1)).build();
-        BookModel bookModel2 = BookModel.builder().name("Преступление и наказание").edition("Как мужик поддался своей жадности").yearPublishing("2000").authors(Set.of(authorModel3)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel2)).build();
-        BookModel bookModel3 = BookModel.builder().name("Мастер и маргарита").edition("Все очень запутано").yearPublishing("2001").authors(Set.of(authorModel4)).categories(Set.of(categoryModel2, categoryModel1)).genres(Set.of(genreModel1, genreModel4)).build();
-        BookModel bookModel4 = BookModel.builder().name("Повесть о настоящем человеке").edition("Название говорит само за себя").yearPublishing("1993").authors(Set.of(authorModel5)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1,genreModel5)).build();
-        BookModel bookModel5 = BookModel.builder().name("Судьба человека").edition("История одного военнослужащего").yearPublishing("1983").authors(Set.of(authorModel6)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1,genreModel2)).build();
-        BookModel bookModel6 = BookModel.builder().name("Собачье сердце").edition("Что значит проводить опыты на людях").yearPublishing("2006").authors(Set.of(authorModel4)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel2)).build();
-        BookModel bookModel7 = BookModel.builder().name("Евгений Онегин").edition("Когда чуть-чуть не успел").yearPublishing("1990").authors(Set.of(authorModel)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1)).build();
+        BookModel bookModel = BookModel.builder().name("Война и мир").edition("Азбука").yearPublishing("1990").authors(Set.of(authorModel1)).categories(Set.of(categoryModel)).genres(Set.of(genreModel)).build();
+        BookModel bookModel1 = BookModel.builder().name("Отцы и дети").edition("СИНБАД").yearPublishing("2006").authors(Set.of(authorModel2)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1)).build();
+        BookModel bookModel2 = BookModel.builder().name("Преступление и наказание").edition("АСТ").yearPublishing("2000").authors(Set.of(authorModel3)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel2)).build();
+        BookModel bookModel3 = BookModel.builder().name("Мастер и маргарита").edition("Вече").yearPublishing("2001").authors(Set.of(authorModel4)).categories(Set.of(categoryModel2, categoryModel1)).genres(Set.of(genreModel1, genreModel4)).build();
+        BookModel bookModel4 = BookModel.builder().name("Повесть о настоящем человеке").edition("Вече").yearPublishing("1993").authors(Set.of(authorModel5)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1,genreModel5)).build();
+        BookModel bookModel5 = BookModel.builder().name("Судьба человека").edition("Белый город").yearPublishing("1995").authors(Set.of(authorModel6)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1,genreModel2)).build();
+        BookModel bookModel6 = BookModel.builder().name("Собачье сердце").edition("Комильфо").yearPublishing("2006").authors(Set.of(authorModel4)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel2)).build();
+        BookModel bookModel7 = BookModel.builder().name("Евгений Онегин").edition("Вита Нова").yearPublishing("1990").authors(Set.of(authorModel)).categories(Set.of(categoryModel1)).genres(Set.of(genreModel1)).build();
         bookRepository.saveAll(Arrays.asList(bookModel,bookModel1,bookModel2,bookModel3,bookModel4,bookModel5,bookModel6,bookModel7));
 
         UsersModel usersModel = UsersModel.builder().login("ikyz9").password("123").firstname("Илья").lastname("Кузнецов").email("ikyz9@gmail.com").city("Саратов").categories(Set.of(categoryModel2, categoryModel)).build();
-        UsersModel usersModel1 = UsersModel.builder().login("tuda").password("suda").firstname("Тутанхомон").lastname("третий").email("tudasuda@gmail.com").city("Новосибирск").categories(Set.of(categoryModel1)).build();
-        UsersModel usersModel2 = UsersModel.builder().login("mimika").password("qwe").firstname("Мим").lastname("Денисов").email("mimika9@gmail.com").city("Санкт-Петербург").categories(Set.of(categoryModel,categoryModel1)).build();
-        UsersModel usersModel3 =UsersModel.builder().login("zxc").password("zxc").firstname("крик").lastname("луны").email("zxc1vs1@gmail.com").city("Челябинск").categories(Set.of(categoryModel2  )).build();
+        UsersModel usersModel1 = UsersModel.builder().login("elizaveta").password("123").firstname("Елизавета").lastname("Василькова").email("tudasuda@gmail.com").city("Новосибирск").categories(Set.of(categoryModel1)).build();
+        UsersModel usersModel2 = UsersModel.builder().login("mimika").password("qwe").firstname("Михаил").lastname("Денисов").email("mimika9@gmail.com").city("Санкт-Петербург").categories(Set.of(categoryModel,categoryModel1)).build();
+        UsersModel usersModel3 =UsersModel.builder().login("zxc").password("zxc").firstname("Юлия").lastname("Чаплыгина").email("zxc1vs1@gmail.com").city("Челябинск").categories(Set.of(categoryModel2  )).build();
         usersRepository.saveAll(Arrays.asList(usersModel,usersModel1,usersModel2,usersModel3));
 
-        NewsFeedModel newsFeedModel = NewsFeedModel.builder().heading("Bookcrossing").body("YEAHHHHH").dateOfPosting("15/05").build();
-        NewsFeedModel newsFeedModel1 = NewsFeedModel.builder().heading("One more year").body("Let`s celebrate").dateOfPosting("01/01").build();
-        NewsFeedModel newsFeedModel2 = NewsFeedModel.builder().heading("Summer").body("Let`s read somethink").dateOfPosting("01/06").build();
+        NewsFeedModel newsFeedModel = NewsFeedModel.builder().heading("Bookcrossing").body("Приложение практически запустилось. Расскажите о нем своим друзьям").dateOfPosting("15/05").build();
+        NewsFeedModel newsFeedModel1 = NewsFeedModel.builder().heading("Лето").body("Лето самая прекрасная пора для чтения. Заварите себе чай и в приятной и уютной обстановке возьмите и перечитайте свою любимую книгу.").dateOfPosting("01/01").build();
+        NewsFeedModel newsFeedModel2 = NewsFeedModel.builder().heading("Соцсети").body("Отмечайте нас после успешного обмена в соцсетях").dateOfPosting("01/06").build();
         newsFeedRepository.saveAll(
                 Arrays.asList((newsFeedModel),
                 (newsFeedModel1),
@@ -99,23 +99,18 @@ public class DataFillingController {
                         BookUserRatingModel.builder().bookModel(bookModel).usersModel(usersModel2).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel).usersModel(usersModel3).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel1).usersModel(usersModel).grade(5).build(),
-                        BookUserRatingModel.builder().bookModel(bookModel1).usersModel(usersModel1).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel1).usersModel(usersModel2).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel1).usersModel(usersModel3).grade(5).build(),
-                        BookUserRatingModel.builder().bookModel(bookModel2).usersModel(usersModel).grade(2).build(),
                         BookUserRatingModel.builder().bookModel(bookModel2).usersModel(usersModel1).grade(1).build(),
                         BookUserRatingModel.builder().bookModel(bookModel2).usersModel(usersModel2).grade(5).build(),
-                        BookUserRatingModel.builder().bookModel(bookModel2).usersModel(usersModel3).grade(3).build(),
                         BookUserRatingModel.builder().bookModel(bookModel4).usersModel(usersModel).grade(3).build(),
                         BookUserRatingModel.builder().bookModel(bookModel4).usersModel(usersModel1).grade(2).build(),
                         BookUserRatingModel.builder().bookModel(bookModel4).usersModel(usersModel2).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel4).usersModel(usersModel3).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel5).usersModel(usersModel).grade(5).build(),
-                        BookUserRatingModel.builder().bookModel(bookModel5).usersModel(usersModel1).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel5).usersModel(usersModel2).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel5).usersModel(usersModel3).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel6).usersModel(usersModel).grade(2).build(),
-                        BookUserRatingModel.builder().bookModel(bookModel6).usersModel(usersModel1).grade(1).build(),
                         BookUserRatingModel.builder().bookModel(bookModel6).usersModel(usersModel2).grade(5).build(),
                         BookUserRatingModel.builder().bookModel(bookModel6).usersModel(usersModel3).grade(3).build(),
                         BookUserRatingModel.builder().bookModel(bookModel7).usersModel(usersModel).grade(4).build(),
@@ -126,15 +121,16 @@ public class DataFillingController {
 
         bookUserCommentRepository.saveAll(
                 Arrays.asList(
-                        BookUserCommentModel.builder().bookModel(bookModel).usersModel(usersModel).comment("норм").build(),
-                        BookUserCommentModel.builder().bookModel(bookModel1).usersModel(usersModel1).comment("очень понравилось").build()
+                        BookUserCommentModel.builder().bookModel(bookModel).usersModel(usersModel).comment("очень приятная серия, хорошая плотная обложка, одна из самых прочных среди книг в мягких переплётах, бумага плотная, шрифт не мизерный. про содержание и говорить не стоит - это классика, обязательная к прочтению.").build(),
+                        BookUserCommentModel.builder().bookModel(bookModel).usersModel(usersModel3).comment("Читается тяжело, на любителя. В школе этого произведения не читала, но и не жалею. Просто хочу для себя прочесть.").build(),
+                        BookUserCommentModel.builder().bookModel(bookModel5).usersModel(usersModel3).comment("Великолепная классика в отличном оформлении! История такая душевная, пронзительная и цепляющая. Только перечитывать и перечитывать").build(),
+                        BookUserCommentModel.builder().bookModel(bookModel5).usersModel(usersModel2).comment("Проза Шолохова не нуждается в особом представлении: чувственная, пронзительная, трогательная. В книгу входит ряд рассказов и повестей автора").build()
                 )
         );
 
         usersBooksRepository.saveAll(
                 Arrays.asList(
-//                        UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel).type("Мои").build(),
-                        UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel3).type("Мои").build(),
+                        UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel).type("Мои").build(),
                         UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel7).type("Мои").build(),
                         UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel2).type("Мои").build(),
                         UsersBooksModel.builder().usersModel(usersModel).bookModel(bookModel6).type("Мои").build(),
